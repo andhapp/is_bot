9bd4add490de54424c6ec230e884c8da488ade07
anuj  dutta <andhapp@andHapps-MacBook.local>
2009-11-19 16:57:32 +0530

    Added module setting up spec premise

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require File.join(File.dirname(__FILE__), "..", "lib", "is_bot")
 
 require 'activerecord'
+require 'action_view'
 require 'yaml'
 require 'spec'
  
@@ -11,3 +12,9 @@ end
  
 connect('test')
 load(File.join(File.dirname(__FILE__), "schema.db"))
+
+module ViewHelper; class ViewField < ActionView::Base; end end
+
+Spec::Runner.configure do |config|
+  config.include(ViewHelper)
+end
